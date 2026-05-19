d3b955006fd1d903b1fcaf95a1951d2e94d0903e
Malik Muhammad Awan <muhammad.dev.pk@gmail.com>
2026-05-19 15:14:27 +0500

    Create 2026_05_19_095143_create_transactions_table.php

---
+++ database/migrations/2026_05_19_095143_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('seller_id')->constrained('users')->cascadeOnDelete();
+            $table->decimal('amount', 10, 2);
+            $table->unsignedInteger('deadline');
+            $table->enum('status', ['pending', 'accepted', 'approved', 'disputed', 'released', 'canceled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
